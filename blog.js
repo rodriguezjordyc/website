4b7549ff6b084a17b62e2ebc3e552643881662be
Jordy Rodriguez <rodriguezjordyc@gmail.com>
2026-02-27 13:35:44 -0800

    s

--- blog.js
+++ blog.js
@@ -1,17 +1,3 @@
-// Theme Management
-const themeToggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme') || 'dark';
-
-// Set initial theme
-document.documentElement.setAttribute('data-theme', currentTheme);
-
-themeToggle.addEventListener('click', () => {
-    const theme = document.documentElement.getAttribute('data-theme');
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
-});
-
 // Copy Email to Clipboard
 function copyEmail() {
     const email = 'jordy@alumni.harvard.edu';
